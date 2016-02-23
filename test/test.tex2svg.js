93265e053839d1432ebf2dfdc1613417dc894cb3
kgryte <kgryte@gmail.com>
2016-02-22 23:54:14 -0800

    Add test cases for input arg validation

---
+++ test/test.tex2svg.js
@@ -0,0 +1,159 @@
+'use strict';
+
+// MODULES //
+
+var tape = require( 'tape' );
+var noop = require( '@kgryte/noop' );
+var tex2svg = require( './../lib/tex2svg.js' );
+
+
+// TESTS //
+
+tape( 'main export is a function', function test( t ) {
+	t.equal( typeof tex2svg, 'function', 'main export is a function' );
+	t.end();
+});
+
+tape( 'the function throws an error if provided a callback argument which is not a function (no options)', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		'5',
+		5,
+		null,
+		NaN,
+		true,
+		undefined,
+		[],
+		{}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			tex2svg( 'y = mx+b', value );
+		};
+	}
+});
+
+tape( 'the function throws an error if provided a callback argument which is not a function (options)', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		'5',
+		5,
+		null,
+		NaN,
+		true,
+		undefined,
+		[],
+		{}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			tex2svg( 'y = mx+b', {}, value );
+		};
+	}
+});
+
+tape( 'the function throws an error if not provided an input string (no options)', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		5,
+		null,
+		NaN,
+		true,
+		undefined,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			tex2svg( value, noop );
+		};
+	}
+});
+
+tape( 'the function throws an error if not provided an input string (options)', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		5,
+		null,
+		NaN,
+		true,
+		undefined,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			tex2svg( value, {}, noop );
+		};
+	}
+});
+
+tape( 'the function throws an error if provided an options argument which is not an object', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		'5',
+		5,
+		null,
+		NaN,
+		true,
+		undefined,
+		[],
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			tex2svg( 'y = mx+b', value, noop );
+		};
+	}
+});
+
+tape( 'the function throws an error if provided an invalid option', function test( t ) {
+	t.throws( foo, TypeError, 'throws type error' );
+	t.end();
+
+	function foo() {
+		tex2svg( 'y = mx+b', {'ex':null}, noop );
+	}
+});
